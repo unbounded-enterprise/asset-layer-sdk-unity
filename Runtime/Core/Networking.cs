dfe20bef1e1fb6366ca9b580d3164aedac577c07
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-10-03 17:06:23 -0700

    update to newtonsoft.json serialization

--- Runtime/Core/Networking.cs
+++ Runtime/Core/Networking.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using AssetLayer.SDK;
 using AssetLayer.SDK.Basic;
 using AssetLayer.SDK.Utils;
@@ -23,8 +24,12 @@ namespace AssetLayer.SDK.Core.Networking
             using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonContent))) {
                 var serializer = new DataContractJsonSerializer(typeof(T), new DataContractJsonSerializerSettings() { UseSimpleDictionaryFormat = true });
                 var obj = (T)serializer.ReadObject(memoryStream);
+                // Debug.Log("parse log: " + GetObjectAsJSON(obj));
                 return obj;
             }
+        }
+        public static T GetContentAsObject2<T>(string jsonContent) {
+            return JsonConvert.DeserializeObject<T>(jsonContent);
         }
         public static string GetObjectAsJSON(object obj) {
             using (MemoryStream memoryStream = new MemoryStream()) {
@@ -34,6 +39,9 @@ namespace AssetLayer.SDK.Core.Networking
                 return Encoding.UTF8.GetString(jsonBytes, 0, jsonBytes.Length);
             }
         }
+        public static string GetObjectAsJSON2(object obj) {
+            return JsonConvert.SerializeObject(obj);
+        }
     }
     #if UNITY_WEBGL || UNITY_ANDROID || UNITY_IOS
         public static class UnityNetworking {
@@ -42,7 +50,7 @@ namespace AssetLayer.SDK.Core.Networking
 
                 if (headers != null) foreach (var header in headers) www.SetRequestHeader(header.Key, header.Value);
                 if (body != null) {
-                    www.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(NetworkingUtils.GetObjectAsJSON(body)));
+                    www.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(NetworkingUtils.GetObjectAsJSON2(body)));
                     www.SetRequestHeader("Content-Type", "application/json");
                 }
                 
@@ -56,10 +64,14 @@ namespace AssetLayer.SDK.Core.Networking
 
                 if (www.result == UnityWebRequest.Result.Success) {
                     Debug.Log("GetResponseUnity: " + www.downloadHandler.text);
-                    return NetworkingUtils.GetContentAsObject<T>(www.downloadHandler.text);
+
+                    T data = NetworkingUtils.GetContentAsObject2<T>(www.downloadHandler.text);
+                    Debug.Log("parse log: " + NetworkingUtils.GetObjectAsJSON2(data));
+
+                    return data;
                 } else {
                     Debug.Log("GetResponseUnity Error: " + www.error + " " + www.downloadHandler.text);
-                    BasicErrorResponse err = NetworkingUtils.GetContentAsObject<BasicErrorResponse>(www.downloadHandler.text); 
+                    BasicErrorResponse err = NetworkingUtils.GetContentAsObject2<BasicErrorResponse>(www.downloadHandler.text); 
                     BasicError error = AssetLayerUtils.ParseBasicError(err);
                     throw error;
                 }
@@ -77,7 +89,7 @@ namespace AssetLayer.SDK.Core.Networking
         public static class BasicNetworking {
             public static async Task<T> GetContentAsObjectAsync<T>(HttpResponseMessage response) {
                 var contentString = await response.Content.ReadAsStringAsync();
-                return NetworkingUtils.GetContentAsObject<T>(contentString);
+                return NetworkingUtils.GetContentAsObject2<T>(contentString);
             }
 
             public static async Task<T> Request<T>(string url, string method = "GET", object body = null, Dictionary<string, string> headers = null) {
@@ -87,7 +99,7 @@ namespace AssetLayer.SDK.Core.Networking
                     HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
                     if (method != "GET" && BasicNetworkingUtils.HttpMethodMap.TryGetValue(method, out HttpMethod httpMethod)) { request.Method = httpMethod; }
                     if (body != null) {
-                        request.Content = new StringContent(NetworkingUtils.GetObjectAsJSON(body), Encoding.UTF8, "application/json");
+                        request.Content = new StringContent(NetworkingUtils.GetObjectAsJSON2(body), Encoding.UTF8, "application/json");
                         if (!client.DefaultRequestHeaders.Contains("Content-Type")) 
                             client.DefaultRequestHeaders.Add("Content-Type", "application/json");
                     }
@@ -97,8 +109,7 @@ namespace AssetLayer.SDK.Core.Networking
                     Debug.Log("GetResponse: " + str);
 
                     if (response.IsSuccessStatusCode) {
-                        T data = await GetContentAsObjectAsync<T>(response);
-                        return data;
+                        return await GetContentAsObjectAsync<T>(response);
                     } else {
                         BasicErrorResponse err = await GetContentAsObjectAsync<BasicErrorResponse>(response); 
                         BasicError error = AssetLayerUtils.ParseBasicError(err);
