f97d9e8fdb4f0a94e4d5543a14e5a47af9125df1
Nadrojiskool <jjbhaveagoodtime@yahoo.com>
2024-01-29 20:15:46 -0800

    collection submission updates

--- Runtime/Core/Users.cs
+++ Runtime/Core/Users.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using AssetLayer.SDK;
 using AssetLayer.SDK.Basic;
 using AssetLayer.SDK.Core.Base;
+using AssetLayer.SDK.Collections;
 using AssetLayer.SDK.Users;
 using AssetLayer.SDK.Utils;
 using UnityEngine;
@@ -18,6 +19,13 @@ namespace AssetLayer.SDK.Core.Users
         
         public async Task<User> GetUser(Dictionary<string, string> headers = null) {
             return (await this.Raw.GetUser(headers)).body.user; }
+        public async Task<(List<Collection>, List<string>)> Collections(UserCollectionsProps props, Dictionary<string, string> headers = null) {
+            if (props.idOnly == true) return (null, (await this.Raw.Collections(props, headers)).Item2.body.collections);
+            else return ((await this.Raw.Collections(props, headers)).Item1.body.collections, null); }
+        public async Task<List<Collection>> GetUserCollections(GetUserCollectionsProps props, Dictionary<string, string> headers = null) {
+            return (await this.Raw.GetUserCollections(props, headers)).body.collections; }
+        public async Task<List<string>> GetUserCollectionIds(GetUserCollectionsProps props, Dictionary<string, string> headers = null) {
+            return (await this.Raw.GetUserCollectionIds(props, headers)).body.collections; }
         public async Task<(string, RegisterUserResponseBody)> Register(RegisterUserProps props, Dictionary<string, string> headers = null) {
             if (props.otp != null) return (null, (await this.Raw.Register(props, headers)).Item2.body);
             else return ((await this.Raw.Register(props, headers)).Item1.body.otp, null); }
@@ -30,6 +38,12 @@ namespace AssetLayer.SDK.Core.Users
 
         public UsersRawHandlers Raw = new UsersRawHandlers {
             GetUser = async (headers) => await _this.Request<GetUserResponse>("/user/info", "GET", null, headers),
+            Collections = async (props, headers) => {
+                if (props.idOnly == true) return (null, await _this.Request<GetUserCollectionIdsResponse>("/user/collections" + AssetLayerUtils.PropsToQueryString(props), "GET", null, headers));
+                else return (await _this.Request<GetUserCollectionsResponse>("/user/collections" + AssetLayerUtils.PropsToQueryString(props), "GET", null, headers), null);
+            },
+            GetUserCollections = async (props, headers) => await _this.Request<GetUserCollectionsResponse>("/user/collections" + AssetLayerUtils.PropsToQueryString(props), "GET", null, headers),
+            GetUserCollectionIds = async (props, headers) => await _this.Request<GetUserCollectionIdsResponse>("/user/collections" + AssetLayerUtils.PropsToQueryString(props, new UserCollectionsProps { idOnly = true }), "GET", null, headers),
             Register = async (props, headers) => {
                 if (props.otp != null) return (null, await _this.Request<RegisterUserResponse>("/user/register", "POST", props, headers));
                 else return (await _this.Request<GetOTPResponse>("/user/register", "POST", props, headers), null);
@@ -42,6 +56,15 @@ namespace AssetLayer.SDK.Core.Users
             GetUser = async (headers) => {
                 try { return new BasicResult<User> { Result = await _this.GetUser(headers) }; }
                 catch (BasicError e) { return new BasicResult<User> { Error = e }; }},
+            Collections = async (props, headers) => {
+                try { return new BasicResult<(List<Collection>, List<string>)> { Result = await _this.Collections(props, headers) }; }
+                catch (BasicError e) { return new BasicResult<(List<Collection>, List<string>)> { Error = e }; }},
+            GetUserCollections = async (props, headers) => {
+                try { return new BasicResult<List<Collection>> { Result = await _this.GetUserCollections(props, headers) }; }
+                catch (BasicError e) { return new BasicResult<List<Collection>> { Error = e }; }},
+            GetUserCollectionIds = async (props, headers) => {
+                try { return new BasicResult<List<string>> { Result = await _this.GetUserCollectionIds(props, headers) }; }
+                catch (BasicError e) { return new BasicResult<List<string>> { Error = e }; }},
             Register = async (props, headers) => {
                 try { return new BasicResult<(string, RegisterUserResponseBody)> { Result = await _this.Register(props, headers) }; }
                 catch (BasicError e) { return new BasicResult<(string, RegisterUserResponseBody)> { Error = e }; }},
