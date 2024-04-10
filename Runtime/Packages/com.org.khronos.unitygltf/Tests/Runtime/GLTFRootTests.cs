2c646cd5e478933519e6cac46af0f531685836f3
Jan <janv83@gmail.com>
2024-04-10 04:25:46 +0200

    included unitygltf directly in the project

---
+++ Runtime/Packages/com.org.khronos.unitygltf/Tests/Runtime/GLTFRootTests.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text;
+using GLTF;
+using NUnit.Framework;
+
+public class GLTFRootTest {
+
+	[Test]
+	public void TestMinimumGLTF()
+	{
+		var testStr = @"
+			{
+				""asset"": {
+					""version"": ""2.0""
+				}
+			}
+		";
+
+		var stream = new MemoryStream(Encoding.UTF8.GetBytes(testStr));
+		GLTFParser.ParseJson(stream, out var testRoot);
+
+		Assert.AreEqual(testRoot.Asset.Version, "2.0");
+	}
+}
